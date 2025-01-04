63acb9310c748f9e8776f2d7b45a5239a9f31cec
danyoh <danyoh#gmail.com>
2025-01-05 01:10:57 +0900

    1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ const eslintConfig = [
       "@next/next/no-page-custom-font": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
       "no-unused-vars": "off",
     },
   },
